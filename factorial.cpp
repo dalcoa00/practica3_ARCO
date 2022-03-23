70b5ecbd15b21ada82aa472d8da7eb5e214bf759
DiegoAlcoba <diegoalcobaarias@gmail.com>
2022-03-23 19:00:47 +0100

    Clase factorial creo que hecha

--- factorial.cpp
+++ factorial.cpp
@@ -1,6 +1,41 @@
 #include "factorial.h"
+#include "mainwindow.h"
 
-Factorial::Factorial()
-{
+Factorial::Factorial() {
 
+}
+
+int Factorial::getNum() {
+    return numero;
+}
+
+void Factorial::setNum() {
+    /* Crea un archivo de texto que contenga el número del que se calculará el factorial */
+
+    ofstream escribe("numero.txt");
+    escribe<<"463783425"; /* Provisional, el archivo ya debe ir en el ejecutable con el número */
+
+    /* Lee el archivo de texto con el número */
+    ifstream lee("numero.txt");
+    getline(lee, num);
+    lee.close();
+
+    Factorial::numero = stoi(num);
+
+}
+
+void Factorial::calculaFactorial() {
+
+    /* Calcula el factorial del número */
+    int numero = this->getNum();
+    int factorial = 1;
+
+    for (int i = 1; i <= numero; i++) {
+
+        factorial = factorial * i;
+    }
+
+    /* Una vez calculado, escribe el resultado en el mismo documento de texto*/
+    ofstream archivo("numero.txt");
+    archivo<<factor<<"\nEl factorial de "<<numero<<" es "<<factorial<<endl;
 }
--- factorial.h
+++ factorial.h
@@ -1,11 +1,28 @@
 #ifndef FACTORIAL_H
 #define FACTORIAL_H
 
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <string>
+#include <mainwindow.h>
+//#include <ui_mainwindow.h> /* Aún no se ha creado */
+
+using namespace std;
 
 class Factorial
 {
-public:
+private:
+    string num, factor;
+    int numero, factorial;
+
+
+public: //Métodos
     Factorial();
+    int getNum();
+    void setNum();
+    void calculaFactorial();
+
 };
 
 #endif // FACTORIAL_H
